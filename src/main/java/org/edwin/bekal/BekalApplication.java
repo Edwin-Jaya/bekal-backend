fd59cdf56c835211a28a7285c234864bc32ca465
Edwin Jaya <91804364+Edwin-Jaya@users.noreply.github.com>
2026-08-10 09:51:46 +0700

    First Commit

---
+++ src/main/java/org/edwin/bekal/BekalApplication.java
@@ -0,0 +1,13 @@
+package org.edwin.bekal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BekalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BekalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/edwin/bekal/BekalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edwin.bekal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BekalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
